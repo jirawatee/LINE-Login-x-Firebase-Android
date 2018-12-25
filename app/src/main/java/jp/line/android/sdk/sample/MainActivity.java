3900d12125320efdeea131346da56aa20c5a5bf7
Jirawat Karanwittayakarn <jirawatee@gmail.com>
2018-12-25 10:20:12 +0700

    LINE SDK 5.0.1

--- app/src/main/java/jp/line/android/sdk/sample/MainActivity.java
+++ app/src/main/java/jp/line/android/sdk/sample/MainActivity.java
@@ -8,8 +8,12 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.linecorp.linesdk.Scope;
+import com.linecorp.linesdk.auth.LineAuthenticationParams;
 import com.linecorp.linesdk.auth.LineLoginApi;
 import com.linecorp.linesdk.auth.LineLoginResult;
+
+import java.util.Arrays;
 
 import jp.line.android.sdk.sample.helpers.FirebaseHelper;
 import jp.line.android.sdk.sample.helpers.PreferenceHelper;
@@ -18,6 +22,7 @@ import jp.line.android.sdk.sample.interfaces.Constants;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 	private static final int REQUEST_CODE_1 = 1;
 	private static final int REQUEST_CODE_2 = 2;
+	private static final String TAG = MainActivity.class.getSimpleName();
 	public TextView btnLoginNative, btnLoginBrowser, btnLoginEmail, txtStatus;
 	private boolean isClicked = false;
 
@@ -33,7 +38,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	@Override
 	protected void onResume() {
 		super.onResume();
+
 		//if (!isClicked) LineHelper.verifyToken(this);
+
 		if (FirebaseAuth.getInstance().getCurrentUser() != null) {
 			startActivity(new Intent(this, PostLoginEmailActivity.class));
 			finish();
@@ -48,13 +55,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	public void onClick(View view) {
 		isClicked = true;
 		Intent LoginIntent;
+
+		LineAuthenticationParams params = new LineAuthenticationParams.Builder()
+				.scopes(Arrays.asList(Scope.OPENID_CONNECT, Scope.OC_EMAIL, Scope.PROFILE))
+				.botPrompt(LineAuthenticationParams.BotPrompt.normal)
+				.build();
+
 		switch (view.getId()) {
 			case R.id.btn_login_native:
-				LoginIntent = LineLoginApi.getLoginIntent(this, Constants.CHANNEL_ID);
+				LoginIntent = LineLoginApi.getLoginIntent(this, Constants.CHANNEL_ID, params);
 				startActivityForResult(LoginIntent, REQUEST_CODE_1);
 				break;
 			case R.id.btn_login_browser:
-				LoginIntent = LineLoginApi.getLoginIntentWithoutLineAppAuth(this, Constants.CHANNEL_ID);
+				LoginIntent = LineLoginApi.getLoginIntentWithoutLineAppAuth(this, Constants.CHANNEL_ID, params);
 				startActivityForResult(LoginIntent, REQUEST_CODE_1);
 				break;
 			case R.id.btn_login_email:
@@ -72,7 +85,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 					LineLoginResult result = LineLoginApi.getLoginResultFromIntent(data);
 					switch (result.getResponseCode()) {
 						case SUCCESS:
-							startActivity(new Intent(this, PostLoginActivity.class));
+							Intent intent = new Intent(this, PostLoginActivity.class);
+							/*
+							intent.putExtra(PostLoginActivity.LINE_LOGIN_USER_ID, result.getLineProfile().getUserId());
+							intent.putExtra(PostLoginActivity.LINE_LOGIN_DISPLAY_NAME, result.getLineProfile().getDisplayName());
+							intent.putExtra(PostLoginActivity.LINE_LOGIN_STATUS_MESSAGE, result.getLineProfile().getStatusMessage());
+							intent.putExtra(PostLoginActivity.LINE_LOGIN_ACCESS_TOKEN, result.getLineCredential().getAccessToken().getTokenString());
+							intent.putExtra(PostLoginActivity.LINE_LOGIN_PICTURE, result.getLineProfile().getPictureUrl().toString());
+							*/
+							intent.putExtra(PostLoginActivity.LINE_LOGIN_EMAIL, result.getLineIdToken().getEmail());
+							startActivity(intent);
 							finish();
 							break;
 						case CANCEL:
@@ -82,6 +104,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 						default:
 							isClicked = false;
 							txtStatus.setText(result.getErrorData().toString());
+							Log.e(TAG, result.getErrorData().toString());
 					}
 					break;
 				case REQUEST_CODE_2:
